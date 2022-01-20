afd903e8572565343be702e2aa37eae5e5a46f82
luyipao <luyipao6@gmail.com>
2022-01-20 03:49:59 -0800

    update

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,6 +1,6 @@
 public class NBody
 {
-    public static String imageToDraw = "images/starfield.jpg";
+    private static String imageToDraw = "images/starfield.jpg";
     
     public static double readRadius(String filename)
     {
