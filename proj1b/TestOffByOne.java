4ce6facec7efe6a2b6269f6a1d0f66f05d39c85f
luyipao <luyipao6@gmail.com>
2022-01-30 06:01:30 -0800

    1/30-3

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -20,7 +20,27 @@ public class TestOffByOne {
 
     }
 
+    @Test
+    public void equalCharsTest1() {
+        OffByN obn1 = new OffByN(1);
+        assertTrue(palindrome.isPalindrome("acdb", obn1));
+        assertTrue(palindrome.isPalindrome("", offByOne));
+        assertTrue(palindrome.isPalindrome("1", offByOne));
+        assertTrue(palindrome.isPalindrome("a", offByOne));
+    }
+    @Test
+    public void equalCharsTest2() {
+        OffByN obn2 = new OffByN(2);
+        assertTrue(palindrome.isPalindrome("acfd", obn2));
+        assertTrue(palindrome.isPalindrome("", offByOne));
+        assertTrue(palindrome.isPalindrome("1", offByOne));
+        assertTrue(palindrome.isPalindrome("a", offByOne));
+
+    }
     public void main(String[] args) {
         equalCharsTest();
+        equalCharsTest1();
+        equalCharsTest2();
+
     }
 }
