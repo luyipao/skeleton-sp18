87bcd4ab324f508d54d4249580b899ca677c3404
luyipao <luyipao6@gmail.com>
2022-01-31 09:01:54 -0800

    1

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -7,19 +7,9 @@ public class TestOffByOne {
     // You must use this CharacterComparator and not instantiate
     // new ones, or the autograder might be upset.
     static CharacterComparator offByOne = new OffByOne();
-    static Palindrome palindrome = new Palindrome();
 
     @Test
     public void equalCharsTest() {
-        assertFalse(palindrome.isPalindrome("1  3", offByOne));
-        assertTrue(palindrome.isPalindrome("1111222", offByOne));
-        assertTrue(palindrome.isPalindrome("a", offByOne));
-        assertTrue(palindrome.isPalindrome("A", offByOne));
-        assertFalse(palindrome.isPalindrome("Aa", offByOne));
-        assertTrue(palindrome.isPalindrome("&", offByOne));
-        assertTrue(palindrome.isPalindrome("flake", offByOne));
-        /** assertTrue(palindrome.isPalindrome("%&", offByOne)); */
-        assertFalse(palindrome.isPalindrome("noon", offByOne));
         assertTrue(offByOne.equalChars('a', 'b'));
         assertFalse(offByOne.equalChars('r', 'Q'));
         assertFalse(offByOne.equalChars('D', 'B'));
