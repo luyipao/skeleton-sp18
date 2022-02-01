500f6eec9c800b4ad221764b2c6ad762a7a3dea0
luyipao <luyipao6@gmail.com>
2022-02-01 05:58:46 -0800

    fail the work

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -1,68 +1,43 @@
-import static org.junit.Assert.*;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class TestArrayDequeGold {
     @Test
-    public void empty_sizeSADTest() {
-        StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
-        assertTrue(sad1.isEmpty());
-        assertEquals(true, sad1.size() == 0);
-        sad1.addFirst(StdRandom.uniform(10));
-        assertFalse(sad1.isEmpty());
-        assertEquals(true, sad1.size() == 1);
-        sad1.removeFirst();//something is wrong
-        assertTrue(sad1.isEmpty());
-        assertEquals(true, sad1.size() == 0);
-        sad1.addLast(StdRandom.uniform(10));//something is wrong
-        assertFalse(sad1.isEmpty());
-        assertEquals(true, sad1.size() == 1);
-        sad1.removeLast();
-        assertTrue(sad1.isEmpty());
-        assertEquals(true, sad1.size() == 0);
-    }
-
-    @Test
     public void add_removeSADTest() {
-        StudentArrayDeque<Integer> sad2 = new StudentArrayDeque<>();
-        Integer x;
-        for (int i = 0; i < 10; i += 1) {
-            double numberBetweenZeroAndOne = StdRandom.uniform(10);
-
-            if (numberBetweenZeroAndOne < 5) {
-                sad2.addFirst(i);
-            } else {
-                sad2.addLast(i);
+        StudentArrayDeque<Integer> test1 = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> test2 = new ArrayDequeSolution<>();
+        Integer studentOutput;
+        Integer correctOutput;
+        while (true) {
+            double numberBetweenZeroAndOne = StdRandom.uniform();
+            int number = StdRandom.uniform(10);
+            if (numberBetweenZeroAndOne > 0.75) {
+                test1.addFirst(number);
+                test2.addFirst(number);
+                System.out.printf("addFirst(%d)\n", number);
+            } else if (numberBetweenZeroAndOne >= 0.5 && numberBetweenZeroAndOne < 0.75) {
+                test1.addLast(number);
+                test2.addLast(number);
+                System.out.printf("addLast(%d)\n", number);
+            } else if (numberBetweenZeroAndOne < 0.5 && numberBetweenZeroAndOne >= 0.25
+                    && !test1.isEmpty() && !test2.isEmpty()) {
+                studentOutput = test1.removeFirst();
+                correctOutput = test2.removeFirst();
+                System.out.printf("removeFirst()\n");
+                assertEquals(correctOutput, studentOutput);
+            } else if (numberBetweenZeroAndOne >= 0 && numberBetweenZeroAndOne < 0.25
+                    && !test1.isEmpty() && !test2.isEmpty()) {
+                studentOutput = test1.removeLast();
+                correctOutput = test2.removeLast();
+                System.out.printf("removeLast()\n");
+                assertEquals(correctOutput, studentOutput);
+            } else  {
+                assertEquals(test2.isEmpty(), test1.isEmpty());
             }
         }
-        do {
-            x = sad2.removeLast();
-        } while (x >= 5);
-        for(int i = 0; i < sad2.size(); i ++) {
-            assertEquals("removeLast()",true,(sad2.get(i) == null || sad2.get(i) < 5));
-        }
     }
-/**
-    @Test
-    public void add_removeADSTest() {
-        ArrayDequeSolution<Integer> ads1 = /*ArrayDequeSolution();
-        for (int i = 0; i < 10; i += 1) {
-            double numberBetweenZeroAndOne = StdRandom.uniform(100);
-
-            if (numberBetweenZeroAndOne < 50) {
-                sad2.addFirst(i);
-            } else {
-                sad2.addLast(i);
-            }
-        }
-        for (int i = 0; i < 5; i++) {
-            assertTrue(sad2.get(i) < 50);
-            sad3.removeLast();
-        }
-        assertTrue(sad3.size() == 5);
-    }
-    */
     public void main(String[] args) {
-        empty_sizeSADTest();
         add_removeSADTest();
     }
 }
